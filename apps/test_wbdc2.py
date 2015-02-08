c745f3b3232462ae7cef2a0dc8bde7f3963e3313
Tom Kuiper <kuiper@JPL.NASA.GOV>
2015-02-08 04:20:58 +0000

    Debugging WBDC code

---
+++ apps/test_wbdc2.py
@@ -0,0 +1,15 @@
+"""
+Testing just through the WBDC
+"""
+from MonitorControl.Configurations import station_configuration
+from MonitorControl.config_test import *
+from support import logs
+                                     
+if __name__ == "__main__":
+  logging.basicConfig(level=logging.INFO)
+  testlogger = logging.getLogger()
+  logs.init_logging(testlogger, loglevel=logging.DEBUG, consolevel=logging.DEBUG)
+
+  observatory, equipment = station_configuration('wbdc2')
+  receiver = equipment['Receiver']
+  receiver.set_IF_mode(SB_separated=True)
